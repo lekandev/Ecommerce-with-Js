e2a863edb702eb474dc16f64d83a79f0ec862280
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-01-19 01:58:06 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,4 +17,8 @@ const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction() {
     // Active link
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
 }
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
